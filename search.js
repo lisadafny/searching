bd787e3e9202301204719f01a5a545c72f04391d
Lisa Dafny <lisadafny.tech@gmail.com>
2022-07-08 21:20:25 -0300

    Empty search bug correction

--- search.js
+++ search.js
@@ -1,4 +1,29 @@
-const clearFieldAfterSearch = (id) => {
-  let input = document.querySelector("#" + id);
+const buttonYoutube = document.getElementById("buttonYoutube");
+
+buttonYoutube.addEventListener("click", function onClick(event) {
+  event.preventDefault();
+
+  document.getElementById("youtubeForm").submit();
+  const input = document.getElementById("youtube");
   input.value = "";
-};
+});
+
+const buttonGoogle = document.getElementById("buttonGoogle");
+
+buttonGoogle.addEventListener("click", function onClick(event) {
+  event.preventDefault();
+
+  document.getElementById("googleForm").submit();
+  const input = document.getElementById("google");
+  input.value = "";
+});
+
+const buttonReddit = document.getElementById("buttonReddit");
+
+buttonReddit.addEventListener("click", function onClick(event) {
+  event.preventDefault();
+
+  document.getElementById("redditForm").submit();
+  const input = document.getElementById("reddit");
+  input.value = "";
+});
